053f4f48f745da949b3df0b3823af731a871ebfa
amineaank <amine.aank@lvdigital.de>
2025-01-20 15:43:47 +0100

    dont import value when the value is already changed in tool

--- src/Actions/SyncPhrasesAction.php
+++ src/Actions/SyncPhrasesAction.php
@@ -2,6 +2,8 @@
 
 namespace Outhebox\TranslationsUI\Actions;
 
+use Illuminate\Support\Facades\Cache;
+use Outhebox\TranslationsUI\Console\Commands\ImportTranslationsCommand;
 use Outhebox\TranslationsUI\Models\Language;
 use Outhebox\TranslationsUI\Models\Translation;
 use Outhebox\TranslationsUI\Models\TranslationFile;
@@ -36,15 +38,23 @@ class SyncPhrasesAction
         ]);
 
         $key = config('translations.include_file_in_key') && ! $isRoot ? "{$translationFile->name}.{$key}" : $key;
-
-        $translation->phrases()->updateOrCreate([
+        $time = Cache::get(ImportTranslationsCommand::CACHE_LAST_IMPORT_TIME_KEY);
+        $existingPhrase = $translation->phrases()->where([
             'key' => $key,
             'group' => $translationFile->name,
             'translation_file_id' => $translationFile->id,
-        ], [
-            'value' => (empty($value) ? null : $value),
-            'parameters' => getPhraseParameters($value),
-            'phrase_id' => $translation->source ? null : $source->phrases()->where('key', $key)->first()?->id,
-        ]);
+        ])->first();
+
+        if (!$existingPhrase || $existingPhrase->updated_at < $time) {
+            $translation->phrases()->updateOrCreate([
+                'key' => $key,
+                'group' => $translationFile->name,
+                'translation_file_id' => $translationFile->id,
+            ], [
+                'value' => (empty($value) ? null : $value),
+                'parameters' => getPhraseParameters($value),
+                'phrase_id' => $translation->source ? null : $source->phrases()->where('key', $key)->first()?->id,
+            ]);
+        }
     }
 }
